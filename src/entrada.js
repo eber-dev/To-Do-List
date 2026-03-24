b6d1554c0b191ee5b46a5d4a8dfbd9f0e4002af0
Eber <www.eberacunaquispe@gmail.com>
2026-03-24 18:53:06 -0500

    Se creo la clase tarea la cual regiustrar las tareas como un objeto

--- src/entrada.js
+++ src/entrada.js
@@ -1,9 +1,12 @@
 export class tarea {
+    static contador = 1;
     constructor(title, descripcion, fecha, priority, notes) {
+        this.id = tarea.contador;
         this.title = title;
         this.descripcion = descripcion;
         this.fecha = fecha;
         this.priority = priority;
         this.notes = notes;
+        tarea.contador++;
     }
 }

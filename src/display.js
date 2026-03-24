a23c75e662ea527651427c377bc02b9656e44604
Eber <www.eberacunaquispe@gmail.com>
2026-03-23 19:57:26 -0500

    Se añadieron los archivos html css y js del proyecto

---
+++ src/display.js
@@ -0,0 +1,5 @@
+export function loadContainer() {
+    const container = document.querySelector(".container");
+    const titulo = document.querySelector(".titulo");
+    const app = document.querySelector(".app");
+}
---
+++ src/entrada.js
@@ -0,0 +1,9 @@
+export class tarea {
+    constructor(title, descripcion, fecha, priority, notes) {
+        this.title = title;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.priority = priority;
+        this.notes = notes;
+    }
+}

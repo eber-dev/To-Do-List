8680fb4a2cd1b8cfbfe69f0a1018a1218d09a371
Eber <www.eberacunaquispe@gmail.com>
2026-03-24 19:07:19 -0500

    Se añadio un array y las funciones que permiten añadir y eliminar

---
+++ src/box.js
@@ -0,0 +1,13 @@
+export let almacen = [];
+
+export function agregarTarea(objeto) {
+    almacen.push(objeto);
+}
+
+export function eliminarTarea(objeto) {
+    almacen.forEach((elemento, indice) => {
+        if (elemento.id == objeto.id) {
+            almacen.splice(indice, 1);
+        }
+    });
+}

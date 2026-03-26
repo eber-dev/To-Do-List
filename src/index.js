edeaf1ae740ab5319a9ae465dc83edeb6b37174a
Eber <www.eberacunaquispe@gmail.com>
2026-03-26 18:00:08 -0500

    Se llama a la funcion crearModal despues de crear el contenido para proceder a acceder al boton y luego abrir el modal al clickear el mismo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import "./styles.css";
 import { loadContainer } from "./display.js";
 import { obtenerDatos } from "./controller.js";
+import { crearModal } from "./display.js";
 
 loadContainer();
+crearModal();
+
+const añadir = document.getElementById("añadir");
+
+añadir.addEventListener("click", () => {
+    const modal = document.getElementById("modal");
+    modal.showModal();
+});
 obtenerDatos();

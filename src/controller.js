0aeb34231a0e51e564c849d86b190220e576f63a
Eber <www.eberacunaquispe@gmail.com>
2026-03-26 18:59:21 -0500

    Se modifico la funcion de obtener datos para que reciba los datos de un formulario seleccionado como argumento asi mismo al recibil los datos estos se almacenan en una array directamente y se imprime en consola para comprobar que todo este funcionando correctamente

--- src/controller.js
+++ src/controller.js
@@ -1,13 +1,15 @@
-export function obtenerDatos() {
-    const seleccionformulario = document.querySelector(".formulario");
-    seleccionformulario.addEventListener("submit", (e) => {
+import { agregarTarea } from "./box.js";
+import { almacen } from "./box.js";
+
+export function procesaDatos(formulario) {
+    formulario.addEventListener("submit", (e) => {
         e.preventDefault();
         const tarea0 = Object.fromEntries(new FormData(e.target));
         let tarea1 = {
             id: crypto.randomUUID().slice(0, 6),
             ...tarea0,
         };
-
-        alert(JSON.stringify(tarea1));
+        agregarTarea(tarea1);
+        console.log(almacen);
     });
 }

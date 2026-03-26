e8b1e73e8dd64acf0c62079bac6c7639a98077ef
Eber <www.eberacunaquispe@gmail.com>
2026-03-26 16:21:02 -0500

    Se añadio un id al obtener un objeto usando spread para copiar el mismo y asignarle un nuevo atributo

--- src/controller.js
+++ src/controller.js
@@ -2,7 +2,12 @@ export function obtenerDatos() {
     const seleccionformulario = document.querySelector(".formulario");
     seleccionformulario.addEventListener("submit", (e) => {
         e.preventDefault();
-        const tarea = Object.fromEntries(new FormData(e.target));
-        alert(JSON.stringify(tarea));
+        const tarea0 = Object.fromEntries(new FormData(e.target));
+        let tarea1 = {
+            id: crypto.randomUUID().slice(0, 6),
+            ...tarea0,
+        };
+
+        alert(JSON.stringify(tarea1));
     });
 }
